5a86755754ffded5408ba4fdb9217a2c4d2d64c0
Kirill Pestov <Pestov196@yandex.ru>
2023-07-14 15:10:45 +0300

    Change commit

---
+++ src/main/java/ru/netology/manager/Manager.java
@@ -0,0 +1,35 @@
+package ru.netology.manager;
+public class Manager {
+    private String[] films = new String[0];
+    private int limit;
+    public Manager() {
+        this.limit = 5;
+    }
+    public Manager(int limit) {
+        this.limit = limit;
+    }
+    public void addFilms(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+    public String[] findAll() {
+        return films;
+    }
+    public String[] findLust() {
+        int resultLength;
+        if (films.length < limit) {
+            resultLength = films.length;
+        }else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class ManagerTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.manager.Manager;
+
+public class ManagerTest {
+
+    @Test
+    public void addFilmTest() {
+        Manager manager = new Manager();
+        manager.addFilms("Бладшот");
+        String[] expected = {"Бладшот"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addFilmsTest() {
+        Manager manager = new Manager();
+        manager.addFilms("Бладшот");
+        manager.addFilms("Вперёд");
+        manager.addFilms("Джентельмены");
+        String[] expected = {"Бладшот", "Вперёд", "Джентельмены"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void lustLimitFilmsTest() {
+        Manager manager = new Manager(4);
+        manager.addFilms("Бладшот");
+        manager.addFilms("Вперёд");
+        manager.addFilms("Джентельмены");
+        manager.addFilms("Человек-Невидимка");
+        manager.addFilms("Троли");
+        manager.addFilms("Номер Один");
+        String[] expected = {"Номер Один", "Троли", "Человек-Невидимка", "Джентельмены"};
+        String[] actual = manager.findLust();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void lustFilmsTest1() {
+        Manager manager = new Manager();
+        manager.addFilms("Бладшот");
+        manager.addFilms("Вперёд");
+        manager.addFilms("Джентельмены");
+        manager.addFilms("Человек-Невидимка");
+        manager.addFilms("Троли");
+        manager.addFilms("Номер Один");
+        String[] expected = {"Номер Один", "Троли", "Человек-Невидимка", "Джентельмены", "Вперёд"};
+        String[] actual = manager.findLust();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void lustFilmsTest2() {
+        Manager manager = new Manager();
+        manager.addFilms("Человек-Невидимка");
+        manager.addFilms("Троли");
+        manager.addFilms("Номер Один");
+        String[] expected = {"Номер Один", "Троли", "Человек-Невидимка"};
+        String[] actual = manager.findLust();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
